1b6a8a48c4f2c149e5299f81ebac52811b3dac3a
Kim-DaHam <times7749@naver.com>
2022-10-24 17:44:36 +0900

    feat: Signup ui

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -15,7 +15,6 @@ function Login() {
             console.log(res);
             setCode(res.code);
         })
-        
     }
     
     return (
@@ -23,7 +22,7 @@ function Login() {
             <StickyHeader/>
             <div className="loginContent w3-display-container">
                 <div className="login-box w3-display-middle">
-                    <img className="logo" src="#" />
+                    <img className="logo"/>
                     <h3>안녕하세요.<br/>웹킷640에 오신 걸<br/>환영합니다.</h3><br/>
                     <a href={KAKAO_AUTH_URL}><div  className="kakao-btn"/></a>
                     <p>아직 회원이 아니신가요? 
--- src/routes/Signup/Signup.js
+++ src/routes/Signup/Signup.js
@@ -9,18 +9,40 @@ function Signup() {
             <StickyHeader/>
             <div className="signup-content w3-display-container">
                 <div className="signup-box w3-display-middle">
-                    <img className="logo" src="#" />
+                    <img className="logo"/>
                     <h3>회원가입</h3><br/><br/>
                     <form id="signup-form" className="w3-display-middle">
-                    <div className="form-group" id="nameInput">
-                        <label>이름:</label>
-                        <input type="text" className="form-control" id="name" name="name"/>
-                    </div>
-                    <div className="form-group" id="emailInput">
-                        <label>이메일:</label>
-                        <input type="password" className="form-control" id="email" name="email"/>
-                    </div>
-                        <div className="w3-center"><button type="submit" class="join-btn">가입하기</button></div>
+                        <div className="form-group" id="nameInput">
+                            <label>이름:</label>
+                            <input type="text" className="form-control" id="name" name="name" placeholder="이름을 입력하세요."/>
+                        </div>
+                        <div className="form-group" id="emailInput">
+                            <label>이메일:</label>
+                            <input type="text" className="form-control" id="email" name="email" placeholder="이메일을 입력하세요."/>
+                        </div>
+                        <div className="form-group" id="affiliInput">
+                            <label>소속:</label><br/>
+                            <select name="affili" id="affili">
+                                    <option value="">-소속-</option>
+                                    <option value="기업">기업</option>
+                                    <option value="학생">학생</option>
+                                    <option value="관리자">관리자</option>
+                            </select>
+                            <input type="text" className="form-control" id="affili" name="affili" placeholder="기관명을 입력하세요."/>
+                            <div id="studentInput">
+                                <input type="text" className="form-control" id="major" name="major" placeholder="학과"/>
+                                <input type="text" className="form-control" id="snum" name="snum" placeholder="학번"/>
+                            </div>
+                        </div>
+                        <div className="form-group" id="pwdInput">
+                            <label>비밀번호:</label>
+                            <input type="password" className="form-control" id="pwd" name="pwd" placeholder="비밀번호를 입력하세요."/>
+                        </div>
+                        <div className="form-group" id="pwdhInput">
+                            <label>비밀번호 확인:</label>
+                            <input type="password" className="form-control" id="pwdh" name="pwdh" placeholder="비밀번호 확인을 입력하세요."/>
+                        </div>
+                        <div className="w3-center"><button type="submit" className="join-btn">가입하기</button></div>
                     </form>
                 </div>
             </div>
