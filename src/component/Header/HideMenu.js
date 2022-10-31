28063ec8616d45797dc4598b796fc81cc4eb53a2
Kim-DaHam <times7749@naver.com>
2022-10-31 13:58:08 +0900

    feat: Add HideMenu.js

---
+++ src/component/Header/HideMenu.js
@@ -0,0 +1,81 @@
+import { React, useEffect, useState, useMemo } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import "./HideMenu.css"
+
+function HideMenu(props) {
+    const [isBoardClick, setIsBoardClick] = useState(false);
+    const [isProcessClick, setIsProcessClick] = useState(false);
+
+    const onClickBtn = (e) => {
+        let name = '';
+        if(e.target.name) name = e.target.name;
+        else name = e.target.parentNode.name;
+        switch(name){
+            case "board":
+                if(isBoardClick) setIsBoardClick(false);
+                else setIsBoardClick(true);
+                break;
+            case "process":
+                if(isProcessClick) setIsProcessClick(false);
+                else setIsProcessClick(true);
+                break;
+        }
+    }
+
+    return(
+        <div id="hideMenu" className="HideMenu">
+            <div className="hide-content">
+                <ul className="menuList">
+                    <li>About us</li>
+                    <li>게시판
+                        <button name="board" onClick={onClickBtn}>
+                            <FontAwesomeIcon name="board" icon={faChevronRight}/>
+                        </button>
+                    </li>
+                    { isBoardClick && 
+                        <ul className="detail-menu">
+                            <li>공지사항</li>
+                            <li>Q&A</li>
+                        </ul>
+                    }
+                    <li>수업정보
+                        <button name="process" onClick={onClickBtn}>
+                            <FontAwesomeIcon name="process" icon={faChevronRight}/>
+                        </button>
+                    </li>
+                    { isProcessClick && 
+                        <ul className="detail-menu">
+                            <li>수업일정</li>
+                            <li>강의자료</li>
+                        </ul>
+                    }
+                    <li>수강후기</li>
+                    <li>Q&A</li>
+                </ul>
+                { !props.isLogin &&
+                    <ul className="user-menu-list">
+                        <li><a href="/login">로그인</a></li>
+                    </ul>
+                }
+                { props.isLogin && !props.isAdmin &&
+                    <ul className="user-menu-list">
+                        <li>{props.user}님, 어서오세요.
+                            <br/><a href="/student">마이페이지</a>
+                        </li>
+                        <li onClick={props.signout}><a href="#">로그아웃</a></li>
+                    </ul>
+                }
+                { props.isLogin && props.isAdmin &&
+                    <ul className="user-menu-list">
+                        <li><a href="/admin">관리페이지</a></li>
+                        <li onClick={props.signout}><a href="#">로그아웃</a></li>
+                    </ul>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default HideMenu;
--- src/service/OAuth.js
+++ src/service/OAuth.js
@@ -1,4 +1,4 @@
-const REDIRECT_KEY = 'http://192.168.227.163:8080/auth/oauth/kakao';
+const REDIRECT_KEY = 'http://localhost:3000/auth/oauth/kakao';
 const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_KEY}&response_type=code`;
