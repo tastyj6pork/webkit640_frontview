4627a0197363fb031909bbaf723d299230c242cf
KIMJEYOON <kjy9252@naver.com>
2022-10-31 22:16:25 +0900

    feat: How showList 필터 처리 억까해서 보류처리 하였음

--- src/routes/Admin/ApplySelector.js
+++ src/routes/Admin/ApplySelector.js
@@ -11,6 +11,7 @@ function ApplySelector({itemes, fileListUp}) {
     return(<div>
         <div className="apply-insert-items">
             <ul>
+                <input className="items-checkbox" type="checkbox"></input>
                 <li className="items-first">{itemes.name}</li>
                 <li className="items-second">{itemes.school}</li>
                 <li className="items-third">{itemes.major}</li>
@@ -19,7 +20,6 @@ function ApplySelector({itemes, fileListUp}) {
                 <li className="items-sixth">{itemes.email}</li>
                 <li className="items-seventh"><button className="items-file">다운로드</button></li>
                 <li className="items-last"><button className="items-btn" onClick={BtnReverse}>제거</button></li>
-                <input className="items-checkbox" type="checkbox"></input>
             </ul>
         </div>
     </div>)
