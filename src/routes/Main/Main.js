3231d99030c4994d0ebbc7048bc13249a84c3819
Kim-DaHam <times7749@naver.com>
2022-11-04 12:13:18 +0900

    feat: Mobile ver. complelte

--- src/routes/Main/Main.js
+++ src/routes/Main/Main.js
@@ -94,7 +94,13 @@ function Main() {
             <Review ref={reviewRef}/>
             <With/>
         </div>
-        <div className="empty-space"></div>
+        <div className="outro w3-display-container">
+            <div className="outro-background"/>
+            <div className="w3-display-middle w3-center">
+                <span>즐거운 코딩의 시작,<br/>웹킷640과 함께 하세요.</span>
+                <p>놓치지 말고 신청하세요!</p>
+            </div>
+        </div>
         <Footer/>
     </div>
     )
