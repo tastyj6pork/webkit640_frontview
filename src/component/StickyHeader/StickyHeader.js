b6a3896fbf7fd1e1243618a97adabf1b8a4f6ec3
Kim-DaHam <times7749@naver.com>
2022-11-04 16:24:37 +0900

    메인페이지 자잘한 거 수정

--- src/component/StickyHeader/StickyHeader.js
+++ src/component/StickyHeader/StickyHeader.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import "./StickyHeader.css"
 
 function StickyHeader() {
+
     return (
         <div className="StickyHeader">
             <nav className="navbar navbar-expand-sm navbar-light">
-                <Link to='/' className="navbar-brand">Logo</Link>
+                <a href="/" className="navbar-brand">Logo</a>
             </nav>
         </div>
     )
