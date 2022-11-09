51f6a3c8c87f468e28c1af58e92626ad8020f8b1
Kim-DaHam <times7749@naver.com>
2022-11-09 09:54:39 +0900

    feat: Add Dday

--- src/component/MainPage/Dday/Dday.js
+++ src/component/MainPage/Dday/Dday.js
@@ -1,46 +1,50 @@
-import { React, useEffect, useState } from "react";
+import { React, useEffect, useState, useCallback } from "react";
 import { Link } from "react-router-dom";
 import "./Dday.css"
+import moment from "moment";
 
 function Dday() {
-    const [masTime, setMasTime] = useState(new Date("2022-12-25"));
-    const [todayTime, setTodayTime] = useState(new Date());
-    const [diffTime, setDiffTime] = useState(masTime - todayTime);
-    const [day, setDay] = useState(Math.floor(diffTime / (1000*60*60*24)));
-    const [hour, setHour] = useState(Math.floor((diffTime / (1000*60*60)) % 24));
-    const [min, setMin] = useState(Math.floor((diffTime / (1000*60)) % 60));
-    const [sec, setSec] = useState(Math.floor(diffTime / 1000 % 60));
+    const [day, setDay] = useState(0);
+    const [hour, setHour] = useState(0);
+    const [min, setMin] = useState(0);
+    const [sec, setSec] = useState(0);
 
-    useEffect(()=> {
-        const countDown = setInterval(()=>{
-            if(parseInt(sec)>0){
-                setSec(parseInt(sec)-1);
-            }
-            if(parseInt(sec)===0){
-                if(parseInt(min)===0){
-                    if(parseInt(hour)===0){
-                        if(parseInt(day)===0){
-                            clearInterval(countDown);
-                        } else {
-                            setDay(parseInt(day)-1);
-                            setHour(parseInt(hour-1));
-                            setMin(59);
-                            setSec(59);
-                        }
-                    }else {
-                        setMin(parseInt(min)-1);
-                        setSec(59);
-                    }
-                    }
-            }
-        },1000);
-    }, [day, min, sec]);
+    const countDownTimer = useCallback((date)=>{
+        let endDate = moment(date);
+        let timer;
+        
+        function showTimer() {
+            try {
+                let todayDate = moment();
+                let diffDate = endDate - todayDate;
+                if(diffDate < 0){
+                    clearInterval(timer);
+                    setDay(0);
+                    setHour(0);
+                    setMin(0);
+                    setSec(0);
+                    return;
+                }
+                setDay(Math.floor(diffDate / (1000*60*60*24)));
+                setHour(Math.floor((diffDate / (1000*60*60)) % 24));
+                if(Math.floor((diffDate / (1000*60)) % 60) < 10) setMin("0"+Math.floor((diffDate / (1000*60)) % 60))
+                else setMin(Math.floor((diffDate / (1000*60)) % 60));
+                setSec(Math.floor(diffDate / 1000 % 60));
+            } catch(e) {console.log(e)}
+        }
+        timer = setInterval(showTimer,1000);
+    }, [day, hour, min, sec]);
 
+    useEffect(()=>{
+        console.log("useEffect");
+        countDownTimer('2022-11-22');
+    })
 
     return (
         <div className="Dday">
             <div className="dday-box">
-                <label className="day">{day}일</label>
+                <label className="day">{day}</label>
+                <span className="day-span">일</span>
                 <label className="hour">{hour}</label>
                 <span>:</span>
                 <label className="min">{min}</label>
