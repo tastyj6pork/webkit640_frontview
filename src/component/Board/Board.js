86dd4c11d46ec23e3f7a1f774f582d7f419c597a
KIMJEYOON <kjy9252@naver.com>
2022-11-10 17:46:05 +0900

    feat: 뭐 이것저것 많이하였습니다

--- src/component/Board/Board.js
+++ src/component/Board/Board.js
@@ -10,59 +10,49 @@ import TableRow from '@mui/material/TableRow';
 import Header from "../../component/Header/Header";
 import Footer from "../../component/Footer/Footer"; 
 import "../Board/Board.css";
+import { useEffect } from 'react';
+import axios from 'axios';
+import { call } from '../../service/ApiService';
+import { API_BASE_URL } from '../../app-config';
+import { useState } from 'react';
 
-const columns = [
-    { id: 'no', label: '번호', minWidth: 100},
-    { id: 'title', label: '제목', minWidth: 250 },
-    {
-      id: 'writer',
-      label: '작성자',
-      minWidth: 140,
-      align: 'right',
-      format: (value) => value.toLocaleString('en-US'),
-    },
-    {
-      id: 'date',
-      label: '등록일',
-      minWidth: 170,
-      align: 'right',
-      format: (value) => value.toLocaleString('en-US'),
-    },
-    {
-      id: 'look',
-      label: '조회',
-      minWidth: 140,
-      align: 'right',
-      format: (value) => value.toFixed(2),
-    },
-  ];
-  
-  function createData(no, title, writer, date,  look) {
-    return { no, title, writer, date, look};
-  }
-  
-  const rows = [
-    createData('India', 'IN', 1324171354, 3287263),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
-  ];
 
 function Board() {
 
+    const [list, setList] = useState([]);
+    const columns = [
+        { id: 'no', label: '번호', minWidth: 100},
+        { id: 'title', label: '제목', minWidth: 250 },
+        {
+          id: 'writer',
+          label: '작성자',
+          minWidth: 140,
+          align: 'right',
+        },
+        {
+          id: 'date',
+          label: '등록일',
+          minWidth: 170,
+          align: 'right',
+        },
+        {
+          id: 'look',
+          label: '조회',
+          minWidth: 140,
+          align: 'right',
+        },
+      ];
+
+      const rows = list;
+      console.log(rows)
+    
         const [page, setPage] = React.useState(0);
         const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+        useEffect(() => {
+            call("/board/list?type=notification", "GET").then((res) => setList(res));
+        }, [])  
+        console.log(list);
       
         const handleChangePage = (event, newPage) => {
           setPage(newPage);
@@ -73,6 +63,14 @@ function Board() {
           setPage(0);
         };
 
+        const BoardChange = () => {
+            window.location.href="/lecturedata";
+        }
+
+        const GoToEdit = () => {
+            window.location.href="/editor";
+        }
+
     return(<div>
         <Header />
         <div className="board-container">
@@ -80,9 +78,10 @@ function Board() {
                 <h1>공지사항</h1>
                 <div className="board-title-btn">
                     <p style={{border:"3px solid black"}}>공지사항</p>
-                    <p>강의자료</p>
+                    <p onClick={BoardChange}>강의자료</p>
                 </div>
                 <div className="board-search-container">
+                    <button className="board-eidit-btn" onClick={GoToEdit}>글 작성</button>
                     <select name="type" className="board-search-select">
                         <option value="제목">제목</option>
                         <option value="작성자">작성자</option>
@@ -113,17 +112,12 @@ function Board() {
                                         .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                                         .map((row) => {
                                             return (
-                                                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                                                    {columns.map((column) => {
-                                                        const value = row[column.id];
-                                                        return (
-                                                            <TableCell key={column.id} align={column.align}>
-                                                                {column.format && typeof value === 'number'
-                                                                    ? column.format(value)
-                                                                    : value}
-                                                            </TableCell>
-                                                        );
-                                                    })}
+                                                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                                                    <TableCell>{row.id}</TableCell>
+                                                    <TableCell>{row.title}</TableCell>
+                                                    <TableCell align="right">{row.writer}</TableCell>
+                                                    <TableCell align="right">{row.writeDate}</TableCell>
+                                                    <TableCell align="right">{row.cnt}</TableCell>
                                                 </TableRow>
                                             );
                                         })}
@@ -133,7 +127,7 @@ function Board() {
                             <TablePagination
                                 rowsPerPageOptions={[10, 25, 100]}
                                 component="div"
-                                count={rows.length}
+                                count={list.length}
                                 rowsPerPage={rowsPerPage}
                                 page={page}
                                 onPageChange={handleChangePage}
