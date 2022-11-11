72fee515047d3c54a278dfcca52a7b479de71289
Kim-DaHam <times7749@naver.com>
2022-11-11 12:10:49 +0900

    feat: footer css 수정

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -11,7 +11,7 @@ function Footer() {
                     <div className="footer-contents">
                         <footer class="w3-container w3-padding-32">
                             <div class="w3-row-padding">
-                                <div class="w3-third" style={{marginRight:"30px"}}>
+                                <div class="w3-third" style={{marginRight:"100px"}}>
                                 <h3>FOOTER</h3>
                                 <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
                                 <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
