63e8b7d1cc4ed3329b8693e772744e8d3b2adcd8
Kim-DaHam <times7749@naver.com>
2022-11-15 17:32:41 +0900

    feat: 카카오 회원가입 시 고정 이메일 수정 완료

--- src/app-config.js
+++ src/app-config.js
@@ -2,7 +2,6 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-console.log("hostname ", hostname);
 if(hostname === "localhost") {
     backendHost = "http://118.67.134.114:8080";
 }
