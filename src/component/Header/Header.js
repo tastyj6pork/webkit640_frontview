41e509bbf023f19b666c4b013146c79ce7f4bede
KIMJEYOON <kjy9252@naver.com>
2022-11-15 11:20:00 +0900

    feat: Add head review board

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -90,6 +90,10 @@ function Header() {
         }
     })
 
+    const ReviewInsert = () => {
+        window.location.href="/review"
+    }
+
     return(
         <div className="Header">
             <div id="navBarIncludeDetail">
@@ -104,7 +108,7 @@ function Header() {
                                     onClick={showDetail}><span>게시판 ∨</span></Link></li>
                                     <li><Link id="have-dmenu02"
                                     onClick={showDetail}><span>모집정보 ∨</span></Link></li>
-                                    <li><Link><span>수강후기</span></Link></li>
+                                    <li><Link><span onClick={ReviewInsert}>수강후기</span></Link></li>
                                     {!isLogin &&
                                         <li className="login-btn" style={{float:"right", marginLeft: "40px"}}>
                                             <a href="/login">로그인</a>
