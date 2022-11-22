5fbef32735b7b4b4a0adac1dcde0a6f6235f79c4
Kim-DaHam <times7749@naver.com>
2022-11-22 16:56:48 +0900

    feat: 포스트 출력

--- src/component/MainPage/RecruitPoster/RecruitPoster.js
+++ src/component/MainPage/RecruitPoster/RecruitPoster.js
@@ -2,13 +2,23 @@ import React, { useEffect, useState } from 'react';
 import "./RecruitPoster.css"
 import StickyHeader from '../../StickyHeader/StickyHeader';
 
-function RecruitPoster(props) {
+function RecruitPoster() {
+    const urlParams = new URL(document.location).searchParams;
+    const [recruitImg, setRecruitImg] = useState(null);
+
+    useEffect(()=>{
+        const urlParams = new URL(document.location).searchParams;
+        const recruitImg = urlParams.get('recruitImg');
+        setRecruitImg(recruitImg);
+    },[])
+
     return (
         <div className="RecruitPoster">
             <StickyHeader/>
             <div className="recruitPoster w3-display-container">
                 <div className="recruitPoster-box w3-display-middle w3-center">
-                    <img src={props.recruitImg} />
+                    <img src={urlParams.get('recruitImg')} />
+                    {console.log(recruitImg)}
                 </div>
             </div>
         </div>
