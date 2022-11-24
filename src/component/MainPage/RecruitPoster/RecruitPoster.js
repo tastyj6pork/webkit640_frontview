b4095cca8844667e462fcaa1f07329ad9d2e068f
songmingyu0506 <ggp04114@naver.com>
2022-11-24 11:16:26 +0900

    api 주소 변경, 게시판 문제 변경, 모집정보 변경

--- src/component/MainPage/RecruitPoster/RecruitPoster.js
+++ src/component/MainPage/RecruitPoster/RecruitPoster.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import "./RecruitPoster.css"
 import StickyHeader from '../../StickyHeader/StickyHeader';
+import { call } from '../../../service/ApiService';
+import { API_BASE_URL } from '../../../app-config';
 
 function RecruitPoster(props) {
+    const [imageUrl ,setImageUrl] = useState();
+    useEffect(()=>{
+        call("/main/data", "GET").then((res)=>{
+            //console.log(res);
+            setImageUrl(res.imagePath);
+        })
+    },[])
     return (
         <div className="RecruitPoster">
             <StickyHeader/>
             <div className="recruitPoster w3-display-container">
                 <div className="recruitPoster-box w3-display-middle w3-center">
-                    <img src={props.recruitImg} />
+                    <img src={imageUrl} />
                 </div>
             </div>
         </div>
